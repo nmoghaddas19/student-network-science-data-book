bf2163dd4c81df059f10b64ec286a3c22f82da75
Alyssa Smith <asmithh99@gmail.com>
2024-09-04 14:38:36 -0400

    fixing git_fixer

--- git_fixer.py
+++ git_fixer.py
@@ -1,5 +1,6 @@
 import os
 import time
+import glob
 
 os.system('git fetch upstream && git diff main upstream/main > diff.diff')
 
@@ -23,11 +24,31 @@ for b in blocks:
     else:
         fnames_to_move.append(fname)
 
+fname_wildcards = []
 for fname in fnames_to_move:
     fname_extension = fname.split('.')[-1]
     fname_prefix = '.'.join(fname.split('.')[:-1])
+    fname_wildcards.append(fname_prefix + '_MODIFIED_*.' + fname_extension)
     new_fname = fname_prefix + '_MODIFIED_{}'.format(str(time.time()).split('.')[0]) + '.' + fname_extension
     os.system('cp {} {}'.format(fname[1:], new_fname[1:]))
 
 os.system('git checkout main')
 os.system('git merge upstream/main')
+
+for wildcard in fname_wildcards:
+    print(wildcard)
+    keepers = []
+    fnames_from_most_recent = sorted([f for f in glob.glob(wildcard[1:])], reverse=True)
+    print(fnames_from_most_recent)
+    for idx in range(1, len(fnames_from_most_recent)):
+        os.system('cmp --silent -- {} {} > mod.txt'.format(
+            fnames_from_most_recent[0], 
+            fnames_from_most_recent[idx],
+        ))
+        with open('mod.txt', 'r') as f:
+            bytes_diff = ''
+            for line in f.readlines():
+                bytes_diff.append(line)
+            print(len(bytes_diff))
+        if len(bytes_diff) == 0:
+            os.system('rm {}'.format(fnames_from_most_recent[idx]))
